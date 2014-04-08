d758623389c23d1353b58c488bffc45c88d07545
Suzanne Atkinson <suzanneatkinson@gmail.com>
2014-04-08 17:04:00 -0400

    Races List Spec & Refactoring

---
+++ app/controllers/races_controller.rb
@@ -0,0 +1,5 @@
+class RacesController < ApplicationController
+	def index
+		@races = ["Race 1", "Race 2", "Race 3"]
+	end
+end
--- spec/features/list_races_spec.rb
+++ spec/features/list_races_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 
 describe "Viewing the list of races" do
 
-it "shows the races" 
+	it "shows the races" do
 
+		visit races_url
+
+		expect(page).to have_text("3 Races")
+		 expect(page).to have_text("Race 1")
+  		expect(page).to have_text("Race 2")
+  		expect(page).to have_text("Race 3")
+
+	end
 end
 
